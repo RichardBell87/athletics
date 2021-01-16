d5a571a56aed8a89309c46862893b7ed713af4c7
AnnaKulai79 <hannahku68@gmail.com>
2021-01-16 12:21:33 +0200

    added three levels: Domain, Infrastructure, Presentation

---
+++ domain/Athletics/INewsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Athletics
+{
+    public interface INewsRepository
+    {
+        News[] GetById(int id);
+        News[] GetAllByTitle(string titlePart);
+        News[] GetAllByDescription(string descriptionPart);
+        News[] GetAllByBodyText(string bodytextPart);
+        News[] GetAllByTag(string tagPart);
+    }
+}
--- domain/Athletics/News.cs
+++ domain/Athletics/News.cs
@@ -6,54 +6,57 @@ namespace Athletics
     {
         public int Id { get; set; }
 
-        public string Author { get; set; }
+        //public string Author { get; set; }
 
-        public string Date { get; set; }
+        //public string Date { get; set; }
 
-        public string LastChangeAuthor { get; set; }
+        //public string LastChangeAuthor { get; set; }
 
-        public string LastChangeDate { get; set; }
+        //public string LastChangeDate { get; set; }
 
-        public string TitleText { get; set; }
+        public string Title { get; set; }
 
-        public string TitleImage { get; set; }
+        //public string TitleImage { get; set; }
 
-        public string DescriptionText { get; set; }
+        public string Description { get; set; }
 
         public string BodyText { get; set; }
 
-        public string BodyImage1 { get; set; }
+        //public string BodyImage1 { get; set; }
 
-        public string BodyImage2 { get; set; }
+        //public string BodyImage2 { get; set; }
 
-        public string BodyImage3 { get; set; }
+        //public string BodyImage3 { get; set; }
 
-        public string BodyVideoLink { get; set; }
+        //public string BodyVideoLink { get; set; }
 
-        public string Tag1 { get; set; }
+        public string Tag { get; set; }
 
-        public string Tag2 { get; set; }
+        //public string Tag2 { get; set; }
 
-        public string Tag3 { get; set; }
+        //public string Tag3 { get; set; }
 
-        public string Tag4 { get; set; }
+        //public string Tag4 { get; set; }
 
-        public string Tag5 { get; set; }
+        //public string Tag5 { get; set; }
 
-        public string Tag6 { get; set; }
+        //public string Tag6 { get; set; }
 
-        public string Tag7 { get; set; }
+        //public string Tag7 { get; set; }
 
-        public string Tag8 { get; set; }
+        //public string Tag8 { get; set; }
 
-        public string Tag9 { get; set; }
+        //public string Tag9 { get; set; }
 
-        public string Tag10 { get; set; }
+        //public string Tag10 { get; set; }
 
-        public News(int id, string titletext)
+        public News(int id, string title, string descrirtion, string bodytext, string tag)
         {
             Id = id;
-            TitleText = titletext;
+            Title = title;
+            Description = descrirtion;
+            BodyText = bodytext;
+            Tag = tag;
         }
     }
 }
